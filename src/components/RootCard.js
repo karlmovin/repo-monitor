3358f2b478c1410b237fb5479d641d1c51f415cd
fredrikfloven <ffloven@gmail.com>
2019-04-26 16:26:53 +0200

    add connect button

--- src/components/RootCard.js
+++ src/components/RootCard.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import Fab from '@material-ui/core/Fab';
+
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import PAT from '@material-ui/icons/Fingerprint';
 import Repo from '@material-ui/icons/Dvr';
-import Config from '../config'
+
 import RootSpinner from './RootSpinner';
+import Config from '../config';
 
 class RootCard extends React.Component {
   state = {
@@ -64,6 +67,12 @@ class RootCard extends React.Component {
             )
           }}
         />
+        <Fab variant="extended" aria-label="connect" className="connect">
+          <InputAdornment position="start">
+            <Connect />
+          </InputAdornment>
+          Connect
+        </Fab>
         {loading && <RootSpinner />}
       </Card>
     );
