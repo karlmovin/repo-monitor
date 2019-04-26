2f3ad5a829cbd8ea7932f7a25a768fb59dcb7d20
simonjaeger <simon.jager@live.com>
2019-04-26 19:03:44 +0200

    add commit run method

--- src/services/GitHubService.js
+++ src/services/GitHubService.js
@@ -4,12 +4,26 @@ export default class GitHubService {
   getRepo(repoowner, reponame, username, password) {
     username = username || repoowner;
     return axios.get(
-      'https://api.github.com/repos/' + repoowner + '/' + reponame,
+      'https://api.github.com/repos/' + repoowner + '/' + reponame + "/commits/f239a5f3f8e71272058a20443c1ce1e12bdb0272/check-runs",
       {
         auth: {
           username: username,
           password: password
         }
+      }
+    );
+  }
+
+  getCommitRuns(repoowner, reponame, commit, username, password) {
+    username = username || repoowner;
+    return axios.get(
+      'https://api.github.com/repos/' + repoowner + '/' + reponame + "/commits/" + commit + "/check-runs",
+      {
+        auth: {
+          username: username,
+          password: password
+        },
+        headers: {'Accept': 'application/vnd.github.antiope-preview+json'}
       }
     );
   }
