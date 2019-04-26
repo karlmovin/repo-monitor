24a5c9d5488b7d9105b187d53b3b7171a2f7da24
fredrikfloven <ffloven@gmail.com>
2019-04-26 19:20:50 +0200

    fix chaos

--- src/services/GitHubService.js
+++ src/services/GitHubService.js
@@ -4,7 +4,7 @@ export default class GitHubService {
   getRepo(repoowner, reponame, username, password) {
     username = username || repoowner;
     return axios.get(
-      'https://api.github.com/repos/' + repoowner + '/' + reponame + "/commits/f239a5f3f8e71272058a20443c1ce1e12bdb0272/check-runs",
+      'https://api.github.com/repos/' + repoowner + '/' + reponame ,
       {
         auth: {
           username: username,
@@ -14,10 +14,10 @@ export default class GitHubService {
     );
   }
 
-  getCommitRuns(repoowner, reponame, commit, username, password) {
+  getCommitRuns(url, commit, username, password) {
     username = username || repoowner;
     return axios.get(
-      'https://api.github.com/repos/' + repoowner + '/' + reponame + "/commits/" + commit + "/check-runs",
+      url + "/commits/" + commit + "/check-runs",
       {
         auth: {
           username: username,
