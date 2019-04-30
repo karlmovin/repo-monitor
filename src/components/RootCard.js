4df3eb17c0fbe202be1662e3146171af22ae5b09
fredrikfloven <ffloven@gmail.com>
2019-04-30 14:14:32 +0200

    add branchname

--- src/components/RootCard.js
+++ src/components/RootCard.js
@@ -33,6 +33,7 @@ class RootCard extends React.Component {
   state = {
     repoowner: Config.repoowner,
     reponame: Config.reponame,
+    branchname: Config.branchname,
     username: Config.username,
     password: Config.password,
     showPassword: false,
@@ -42,6 +43,7 @@ class RootCard extends React.Component {
     repository: null,
     repo_url: null,
     clone_url: null,
+    ssh_url: null,
     repo_status: null,
     author: null,
     committer: null,
@@ -50,7 +52,6 @@ class RootCard extends React.Component {
     commit_status: null,
     last_update: null,
     commit_url: null,
-    ci_name: null,
     ci_url: null
   };
 
@@ -67,6 +68,9 @@ class RootCard extends React.Component {
     this.state.reponame = urlParams.has('reponame')
       ? urlParams.get('reponame')
       : this.state.reponame;
+    this.state.branchname = urlParams.has('branchname')
+      ? urlParams.get('branchname')
+      : this.state.branchname;
     this.state.username = urlParams.has('username')
       ? urlParams.get('username')
       : this.state.username;
@@ -79,11 +83,25 @@ class RootCard extends React.Component {
   }
 
   componentDidMount() {
-    const { repoowner, reponame, username, password, autoConnect } = this.state;
+    const {
+      repoowner,
+      reponame,
+      branchname,
+      username,
+      password,
+      autoConnect
+    } = this.state;
 
     // If all the parameters are set, and autoConnect is set - trigger
     // click right away.
-    if (repoowner && reponame && username && password && autoConnect) {
+    if (
+      repoowner &&
+      reponame &&
+      branchname &&
+      username &&
+      password &&
+      autoConnect
+    ) {
       this.handleClick();
     }
   }
@@ -100,8 +118,12 @@ class RootCard extends React.Component {
     console.log('TODO');
   };
 
+  handleClickRedirect = () => {
+    console.log('TODO');
+  };
+
   handleClick = () => {
-    const { repoowner, reponame, username, password } = this.state;
+    const { repoowner, reponame, branchname, username, password } = this.state;
     this.setState({ loading: true, error: null });
 
     // TODO: Call GitHub.
@@ -110,20 +132,23 @@ class RootCard extends React.Component {
       .getRepo(repoowner, reponame, username, password)
       .then(
         function(response) {
+          console.log(response.data);
+          var data = response.data;
           this.setState({
             loading: false,
-            repository: response.data,
-            clone_url: response.data.clone_url,
-            repo_url: response.data.html_url,
-            last_update: response.data.updated_at
+            repository: data,
+            clone_url: data.clone_url,
+            ssh_url: data.ssh_url,
+            repo_url: data.html_url,
+            last_update: data.updated_at
           });
 
           service
-            .getCommit(repoowner, reponame, 'master', username, password)
+            .getCommit(repoowner, reponame, branchname, username, password)
             .then(
               function(response) {
                 var data = response.data;
-                console.log(data);
+                //console.log(response);
                 this.setState({
                   latest_commit_sha: data.sha,
                   commit_url: data.html_url,
@@ -135,18 +160,17 @@ class RootCard extends React.Component {
                   .getCommitRuns(
                     repoowner,
                     reponame,
-                    'master',
+                    branchname,
                     username,
                     password
                   )
                   .then(
                     function(response) {
                       var data = response.data.check_runs[0];
-                      //console.log(data);
+                      //console.log(response);
                       this.setState({
-                        ci_url: null || data.details_url,
-                        ci_name: null || data.app.name,
-                        commit_status: data.conclusion
+                        ci_url: data.details_url,
+                        commit_status: data.status
                       });
                     }.bind(this)
                   );
@@ -167,7 +191,14 @@ class RootCard extends React.Component {
   };
 
   renderConnect() {
-    const { repoowner, reponame, username, password, error } = this.state;
+    const {
+      repoowner,
+      reponame,
+      branchname,
+      username,
+      password,
+      error
+    } = this.state;
 
     return (
       <div>
@@ -189,6 +220,19 @@ class RootCard extends React.Component {
           label="Repository Name"
           value={reponame}
           onChange={this.handleChange('reponame')}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Repo />
+              </InputAdornment>
+            )
+          }}
+        />
+        <TextField
+          className="text-field"
+          label="Branch Name"
+          value={branchname}
+          onChange={this.handleChange('branchname')}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -281,25 +325,25 @@ class RootCard extends React.Component {
           <Typography component="div" noWrap>
             {this.state.message}
           </Typography>
-          <Typography>
-            Sha: {JSON.stringify(this.state.latest_commit_sha).substring(1, 8)}
-          </Typography>
-          <a href={this.state.commit_url}>
-            <Code />
-          </a>
-          <br />
-          {this.state.committer}
-          {' committed at '}
-          {this.state.last_update}
           <Chip
-            color="default"
-            avatar={
-              <Avatar>
-                <Success />
-              </Avatar>
-            }
-            label={this.state.commit_status}
+            label={JSON.stringify(this.state.latest_commit_sha).substring(1, 8)}
           />
+          <Chip label={<Code />} onClick={this.handleClickRedirect} />
+          <div>
+            {this.state.committer}
+            {' committed at '}
+            {this.state.last_update}
+            <Chip
+              color="default"
+              avatar={
+                <Avatar>
+                  <Success />
+                </Avatar>
+              }
+              label={this.state.commit_status}
+              onClick={this.handleClickRedirect}
+            />
+          </div>
         </Card>
       </div>
     );
